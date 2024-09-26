d905af1b747d94d82ed3f456e90ad5e769b2f965
Brett Leonard <bmleonard2004@gmail.com>
2024-09-26 09:57:32 -0400

    No more diagonals
    Rewrote the movement to only allow for left, right, up, down.

--- Scenes/Characters/Player/Scripts/player.gd
+++ Scenes/Characters/Player/Scripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 # Change this value to adjust how fast the character moves
 var speed = 200
 
+var moving = false
+var movdir = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -14,11 +17,27 @@ func _ready() -> void:
 # 		i.e. speed = 10 * delta (10 pixels per second)
 # 		diagram of how it works: https://imgur.com/a/NRzrdtG
 func _process(delta: float) -> void:
-	# movement
-	var direction = Input.get_vector("left","right","up","down")
-	position += direction * speed * delta
-	
-	# interaction
-	if (Input.is_action_just_pressed("primary_action")):
-		print("Pew Pew")
+	# User Input
+	if(Input.is_anything_pressed()):
+		if (Input.is_action_pressed("left") && (moving == false || movdir == 1)):
+			position.x -= speed * delta
+			moving = true
+			movdir = 1
+		elif (Input.is_action_pressed("right") && (moving == false || movdir == 2)):
+			position.x += speed * delta
+			moving = true
+			movdir = 2
+		elif (Input.is_action_pressed("down") && (moving == false || movdir == 3)):
+			position.y += speed * delta
+			moving = true
+			movdir = 3
+		elif (Input.is_action_pressed("up") && (moving == false || movdir == 4)):
+			position.y -= speed * delta
+			moving = true
+			movdir = 4
+		else:
+			moving = false
+			movdir = 0
+		if (Input.is_action_just_pressed("primary_action")):
+			print("Pew Pew")
 	
