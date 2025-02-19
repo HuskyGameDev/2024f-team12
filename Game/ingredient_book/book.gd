daa7602d1f0bc5ea140a0ac0df50da3201cc6e8f
dkrontz <dckrontz@gmail.com>
2025-02-19 14:22:22 -0500

    Notebook Final Iteration & Sound Workings

--- Game/ingredient_book/book.gd
+++ Game/ingredient_book/book.gd
@@ -2,17 +2,13 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("J"):
-		if hidden:
+		if !visible:
 			show()
-	if Input.is_action_just_pressed("ui_cancel"):
-		if visible:
+		elif visible:
 			hide()
 
 func _on_button_pressed() -> void:
-	show()
-
-func _on_j_pressed() -> void:
-	show()
-
-func _on_exit_pressed() -> void:
-	hide()
+	if !visible:
+		show()
+	elif visible:
+		hide()
