dcf63f032cd24d17faccbc0ce1a14bf232838cdd
Brett Leonard <bmleonard2004@gmail.com>
2025-04-16 14:58:46 -0400

    Ladder interaction

---
+++ Game/levels/LevelThree/ladder.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@onready var text_box = $"../Cutscene"
+@onready var interaction_area: InteractionArea = $InteractionArea
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	interaction_area.interact = Callable(self, "_on_interact")
+
+func _on_interact():
+	if text_box.time_to_output == false:
+		_send_text()
+
+func _send_text():
+	text_box.queue_text("Moreau", "\"Looks like it would break from the weight of a feather. Probably shouldn't try climbing it.\"")
+	position.x = 918
+	position.y = -187
+	text_box.time_to_output = true
+	pass
