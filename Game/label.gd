cb28749a5d8a84f089033f2e10510d732d9dcd0d
dkrontz <dckrontz@gmail.com>
2025-04-16 19:51:25 -0400

    Adjustments

--- Game/label.gd
+++ Game/label.gd
@@ -7,17 +7,25 @@ var credits3 = "Artists:\nJosiah Parrott\nGabe Newell"
 var credits4 = "Writer:\nDustin Krontz"
 var credits5 = "Sound Team:\nNate Lyons\nZach Brouckman"
 var credits6 = "Music Team:\nZach Broukman"
-var credits7 = "Thank you for playing!"
+var credits7 = "Thank you for playing!!!"
 
 
 func _ready() -> void:
 	scroll_text(credits)
+	await get_tree().create_timer(10).timeout
 	scroll_text(credits2)
+	await get_tree().create_timer(10).timeout
 	scroll_text(credits3)
+	await get_tree().create_timer(10).timeout
 	scroll_text(credits4)
+	await get_tree().create_timer(10).timeout
 	scroll_text(credits5)
+	await get_tree().create_timer(10).timeout
 	scroll_text(credits6)
+	await get_tree().create_timer(10).timeout
 	scroll_text(credits7)
+	await get_tree().create_timer(10).timeout
+	
 
 
 func scroll_text(input_text : String) -> void:
@@ -26,6 +34,5 @@ func scroll_text(input_text : String) -> void:
 	
 	for i in text.length():
 		visible_characters += 1
-		await get_tree().create_timer(0.2).timeout
+		await get_tree().create_timer(0.1).timeout
 	
-	visible_characters = 0
